af82d564233451bd32171ddd4f8505420c73351b
Jim Hillhouse <jdhouse4@me.com>
2020-07-30 17:38:57 -0500

    Initial Commit

---
+++ SceneKitSwiftUIGesturesDemoApp/SceneKitSwiftUIGesturesDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneKitSwiftUIGesturesDemoAppApp.swift
+//  SceneKitSwiftUIGesturesDemoApp
+//
+//  Created by James Hillhouse IV on 7/30/20.
+//
+
+import SwiftUI
+
+@main
+struct SceneKitSwiftUIGesturesDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
